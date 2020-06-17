f5f0f5475aaece62f6e5c63a7966961d00ca2f09
Reid00 <38450639+Reid00@users.noreply.github.com>
2020-06-17 16:07:34 +0800

    update part params

--- dq.py
+++ dq.py
@@ -1,3 +1,7 @@
+"""
+对csv 数据进行dq 检测
+"""
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,20 +51,35 @@ class DQ:
         if data is None:
             data = self.data
 
-        coverage = pd.DataFrame()
+        percentage = list()
         for column in self.columns:
             missing = data[column].isnull().sum()
-            percentage = 1 - round(missing / data.shape[0],2)
-            coverage[column] = [percentage]
+            percentage.append(1 - round(missing / data.shape[0],2))
+        coverage = pd.DataFrame({
+            'column':self.columns,
+            'percentage':percentage
+        })
         coverage.to_csv(f'./dq/coverage_{self.name}.csv',index=False,encoding='utf8')
 
-        fig = plt.figure(figsize=(16,7))
+        fig = plt.figure(figsize=(16,8))
         ax = fig.add_subplot(111)
-        coverage.plot.bar(ax=ax)
+        # or 上面两句等价于 fig,ax = plt.subplots(figsize=(16,8))
+        sns.barplot(x='column', y='percentage',data=coverage,ax=ax)
+        # coverage.plot.bar(ax=ax)
         for p in ax.patches:        # 显示柱状图上的数值
-            ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
-        ax.set_title('各个属性覆盖率')
-        ax.legend(loc='best')
+            ax.annotate(str(round(p.get_height(),2)), (p.get_x() * 1.005, p.get_height() * 1.005))
+
+        ax.set_title('各个属性覆盖率', backgroundcolor='#3c7f99',fontsize=30, weight='bold',color='white')
+        plt.box(False)
+        ax.tick_params(labelsize=16,length=0)  #设置x,y tick(刻度) 的字体大小为16，刻度线长度为0
+        # ax.legend(loc='best')
+        # 设置y轴网格线
+        ax.yaxis.grid(linewidth=0.5, color='black')
+        ax.set_axisbelow(True) # 将网格线置于底部
+        ax.set_xlabel('')      # x轴标签设置为空
+        # x轴ticks 设置
+        plt.xticks(rotation='45', fontsize=10,color='red')
+        plt.subplots_adjust(bottom=0.2) #因为竖着字太长，生成图片中的x轴标签会被截取。因此设置距离底部0.2
         plt.savefig(f'./dq/coverage_{self.name}.png')
         plt.close()
         # plt.show()
@@ -82,21 +101,35 @@ class DQ:
             ####################
             val_cnts = data[column].value_counts().head(7)
             fig = plt.figure(figsize=(16,7))
-            fig.suptitle(f'{column}的value的统计')
+            fig.suptitle(f'{column}的value count的统计',backgroundcolor='#3c7f99',fontsize=30, weight='bold',color='red')
             ax1 = fig.add_subplot(121)
             val_cnts.plot.barh(ax=ax1)
+            # sns.barplot(x=val_cnts.values, y=val_cnts.index,data=val_cnts)
+            # sns.barplot(x=val_cnts.values, y=val_cnts.index.tolist(),ax=ax1)
             for i, v in enumerate(val_cnts.values): # 显示柱状图上的数值
                 ax1.text(v,i, str(v), color='r', fontweight='bold')
+            plt.box(False)
+            # 设置value 的网格线
+            ax1.xaxis.grid(linewidth=0.5, color='black')
+            ax1.set_axisbelow(True)
+            plt.yticks(rotation='45')
             ax1.set_title(f'{column}的value的数量')
             #####################
-                # 百分比 #
+                # 百分比绘图 #
             ####################
             percentage = round(self.data[column].value_counts().head(7)/ len(self.data),2)
             ax2 = fig.add_subplot(122)
             percentage.plot.barh(ax=ax2) 
+            # sns.barplot(x=percentage.values,y=percentage.index,ax=ax2)
             for i, v in enumerate(percentage.values): # 显示柱状图上的数值
                 ax2.text(v,i, str(v), color='r', fontweight='bold')
+            plt.box(False)
+            # 设置value 的网格线
+            ax2.xaxis.grid(linewidth=0.5, color='black')
+            ax2.set_axisbelow(True)
             ax2.set_title(f'{column}的value百分比')  
+            plt.yticks(rotation='45')
+            plt.subplots_adjust(left=0.1)
             # plt.show()
             column = column.replace(':','')
             plt.savefig(f'./dq/values_{column}_{self.name}.png')
@@ -135,9 +168,9 @@ if __name__ == "__main__":
     # paths = get_paths_from_folder(r'E:\GitRepository\KG_Rebuild\ParseLiteratureXML\parsed_res','csv')
     # for path in paths:
     #     exploratory_data_analysis(path)
-    path = Path(r'E:\GitRepository\KG_Rebuild\ParseLiteratureXML\parsed_res\pubmed20n1015.csv')
+    path = Path(r'F:\Literature\test_import\pubmed20n0627.csv')
     dq = DQ(path)
-    data = dq.clean_data()
+    # data = dq.clean_data()
     # dq.value_counts_property(data)
 
     # dq.show_column_length()
